caee28386d87007d5a89b5c7e78f166b6e14e2c2
Jenifer Castaño Ledesma <jennifercasled@gmail.com>
2025-12-06 22:38:04 -0500

    .

--- src/main/java/com/reserve/events/application/ReserveService.java
+++ src/main/java/com/reserve/events/application/ReserveService.java
@@ -30,7 +30,6 @@ public class ReserveService {
     private final AdittionalRepository adittionalRepository;
 
     // TO DO: Verificar que los invitados no excedan el cupo max del establecimiento
-    // TO DO: Agregar los errores que no están al global exception (curso)
     // TO DO: Hacer el get de los servicios y la reserva
     // TO DO: Revisar que las fechas dadas en la lista de Dates sean del presente o del futuro, que no se puedan fechas pasadas (check)
     // TO DO: Revisar colección payments
